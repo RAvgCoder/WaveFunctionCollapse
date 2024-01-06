f592cc629d03fc4adf7f7164af5e5117cf766b01
egbor <egborosebhulimen8@gmail.com>
2024-01-06 17:58:39 -0400

    work in progress

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 #include <memory>
 #include <sstream>
 #include <string>
@@ -8,6 +7,7 @@
 #include "../headers/FileParser.h"
 #include "../headers/ImageFactory.h"
 
+void load_images(std::string absolute_path);
 
 int main() {
     std::cout << "Input the absolute file path to the image" << std::endl;
@@ -16,10 +16,24 @@ int main() {
     std::string absolute_path;
     std::cin >> absolute_path;
 
-    const std::unique_ptr<ImageFactory> image_factory = processImages(absolute_path);
+    load_images(absolute_path);
+
+    const std::unique_ptr<ImageFactory> image_factory = create_image_factory();
 
     image_factory->display();
 
-
     return 0;
+}
+
+void load_images(std::string absolute_path) {
+    // Python
+    std::string python_cmd_runner;
+    const std::string PYTHON_EXE = R"(python C:\Users\egbor\CLionProjects\WaveFunctionCollapse\src\ImageLoader.py)";
+
+    // python_cmd_runner = PYTHON_EXE + " " + absolute_path;
+    python_cmd_runner = PYTHON_EXE + " " + R"(C:\\Users\\egbor\\CLionProjects\\WaveFunctionCollapse\\resources)";
+
+    const char* exe = python_cmd_runner.c_str();
+
+    system(exe);
 }
